ff23018fb770e31173159ecf666344d8ccd83972
Jaime García O Dowd <jgarciaodowd@victoria130.ad.danielcastelao.org>
2019-10-08 09:51:49 +0200

    Inicio

---
+++ boletin1_2/Boletin1_2.java
@@ -0,0 +1,15 @@
+
+package boletin1_2;
+
+public class Boletin1_2 {
+
+    
+    public static void main(String[] args) {
+        //Calcular area cadrado
+        float lado =3;
+        float area =lado*lado;
+        System.out.println("area =" + area);
+        
+    }
+    
+}
